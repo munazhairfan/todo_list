03b254f6e1274d1e22cdf25eac199e0125c5493e
munazhairfan <164248882+munazhairfan@users.noreply.github.com>
2024-05-15 22:26:10 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+let todos= []
+for(let i=0;i<11;++i){
+    let answer = await inquirer.prompt([{
+    name: 'q1',
+    type : 'input',
+    message:'What would you like to add in your list?'}]);
+let items = todos.push(answer.q1);
+
+console.log(todos)
+};
+console.log("You can only add 10 items!");
+console.log("THANK YOU!");
